e73bda4fbb8a1ce480bf6639e38cf1e4d6e2c7aa
Dustinouwu <dastin.chavez@epn.edu.ec>
2022-12-15 14:20:58 -0500

    CardsHechas

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,74 +1,74 @@
 import React, { useState } from 'react';
-import AppBar from '@mui/material/AppBar';
+import Imagenes from "../../Imagenes";
 import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import Link from '@mui/material/Link';
+import CircleIcon from '@mui/icons-material/Circle';
+
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CardActionArea } from '@mui/material';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const commonStyles = {
-  
+
   borderColor: '#000',
   borderWidth: 3,
-  
+
 };
 
 
 
 export default function MultiActionAreaCard() {
-  const variable = 2;
-  const commonStyles = {
-  
-    borderColor: '#000',
-    borderWidth: 3,
-    
-  };
+  const variable = 11;
 
 
   const colorproduct = variable > 10 ? "#FF0000" : "#0FFF18";
   return (
-    <Container sx={{ py: 7 }} maxWidth="lg">
-      {/* End hero unit */}
-      <Grid container spacing={9}>
+    <Container sx={{ py: 5 }} maxWidth="lg">
+      <Grid container spacing={12}>
         {cards.map((card) => (
-          <Grid item key={card} xs={12} sm={8} md={3}>
+          <Grid item key={card} xs={4} sm={4} md={3} >
             <Card
-              sx={{ height: '100%', display: 'flex', flexDirection: 'column', ...commonStyles }}
+              sx={{
+                height: '100%',
+                maxWidth: '250px',
+                display: 'flex',
+                flexDirection: 'column',
+                borderRadius: 3,
+                border: 3,
+              }}
             >
+              <CardActionArea>
               <CardMedia
                 component="img"
                 sx={{
                   // 16:9
-                  pt: '10%',
-                  pb: '10%',
+                  py: '5%',
                 }}
-                image="https://source.unsplash.com/random"
+                image={Imagenes.img5}
                 alt="random"
               />
-              <CardContent sx={{ flexGrow: 1 }}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Heading
+              <CardContent sx={{ flexGrow: 2 }}>
+                <Typography variant="h7" component="h3">
+                  $ 1000
                 </Typography>
-                <Typography>
-                  This is a media card. You can use this section to describe the
-                  content.
+                <Typography variant="h5" component="h5">
+                  IPhone 10
                 </Typography>
+                <div className="rtcontainer" style={{display: 'flex'}}>
+                  <CircleIcon style={{ color: colorproduct, paddingRight: '10px',width: '20' }} />
+                  <Typography noWrap style={{paddingTop: '2px'}} >
+                    Usado
+                  </Typography>
+                </div>
+
               </CardContent>
-              <CardActions>
-                <Button size="small">View</Button>
-                <Button size="small">Edit</Button>
-              </CardActions>
+              </CardActionArea>
             </Card>
           </Grid>
         ))}
